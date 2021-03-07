5fb868ef4d34f93345d059a88d1ac00afc10d4e7
BelyakovArkadiy <belyakovarkadiy@gmail.com>
2021-03-07 20:48:55 +0600

    modified:   src/test/java/ru/netology/web/CardOrderTest.java

--- src/test/java/ru/netology/web/CardOrderTest.java
+++ src/test/java/ru/netology/web/CardOrderTest.java
@@ -1,20 +1,50 @@
 package ru.netology.web;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.support.Color;
 
 import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 class CardOrderTest {
     @Test
-    void shouldTest() {
+    void shouldTest1() {
         open("http://localhost:9999");
         $("[data-test-id=name] input").setValue("Василий Алибабаевич");
         $("[data-test-id=phone] input").setValue("+79270000000");
         $("[data-test-id=agreement]").click();
         $("[type=button]").click();
         $("[data-test-id=order-success]").shouldHave(exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+    @Test
+    void shouldTest2() {
+        open("http://localhost:9999");
+        $("[data-test-id=name] input").setValue("John Doe");
+        $("[data-test-id=phone] input").setValue("+79270000000");
+        $("[data-test-id=agreement]").click();
+        $("[type=button]").click();
+        $("[class=input__sub]").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void shouldTest3() {
+        open("http://localhost:9999");
+        $("[data-test-id=name] input").setValue("Василий Алибабаевич");
+        $("[data-test-id=phone] input").setValue("+792700000000");
+        $("[data-test-id=agreement]").click();
+        $("[type=button]").click();
+        $("[data-test-id=phone] [class=input__sub]").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+    @Test
+    void shouldTest4() {
+        open("http://localhost:9999");
+        $("[data-test-id=name] input").setValue("Василий Алибабаевич");
+        $("[data-test-id=phone] input").setValue("+79270000000");
+//        $("[data-test-id=agreement]").click();
+        $("[type=button]").click();
+        String actual = $(".input_invalid").getCssValue("color");
+        String expected = "#ff5c5c";
+        org.junit.jupiter.api.Assertions.assertEquals(actual, Color.fromString(expected).asRgba());
     }
 }
 
