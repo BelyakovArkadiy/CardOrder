5db18292348dc2ec160cedf79dffcb8f4379ca5a
BelyakovArkadiy <belyakovarkadiy@gmail.com>
2021-03-07 21:43:54 +0600

    modified:   src/test/java/ru/netology/web/CardOrderTest.java

--- src/test/java/ru/netology/web/CardOrderTest.java
+++ src/test/java/ru/netology/web/CardOrderTest.java
@@ -23,7 +23,7 @@ class CardOrderTest {
         $("[data-test-id=phone] input").setValue("+79270000000");
         $("[data-test-id=agreement]").click();
         $("[type=button]").click();
-        $("[class=input__sub]").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $("[.input__sub]").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
     @Test
@@ -33,14 +33,14 @@ class CardOrderTest {
         $("[data-test-id=phone] input").setValue("+792700000000");
         $("[data-test-id=agreement]").click();
         $("[type=button]").click();
-        $("[data-test-id=phone] [class=input__sub]").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+        $("[data-test-id=phone] [.input__sub]").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
     @Test
     void shouldTest4() {
         open("http://localhost:9999");
         $("[data-test-id=name] input").setValue("Василий Алибабаевич");
         $("[data-test-id=phone] input").setValue("+79270000000");
-//        $("[data-test-id=agreement]").click();
+       /* $("[data-test-id=agreement]").click();*/
         $("[type=button]").click();
         String actual = $(".input_invalid").getCssValue("color");
         String expected = "#ff5c5c";
