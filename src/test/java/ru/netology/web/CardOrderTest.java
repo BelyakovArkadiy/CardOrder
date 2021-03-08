e6210e29079adbcde80c0bcb18ac29145f59b703
BelyakovArkadiy <belyakovarkadiy@gmail.com>
2021-03-08 16:44:02 +0600

    modified:   src/test/java/ru/netology/web/CardOrderTest.java

--- src/test/java/ru/netology/web/CardOrderTest.java
+++ src/test/java/ru/netology/web/CardOrderTest.java
@@ -68,5 +68,13 @@ public void setUp() {
         $("[data-test-id=phone] [class=input__sub]").shouldHave(exactText("Поле обязательно для заполнения"));
 
     }
+    @Test
+    void shouldTestAllFieldsButOnlyName() {
+        $("[data-test-id=name] input").setValue("Василий");
+        $("[data-test-id=phone] input").setValue("+79270000000");
+        $("[data-test-id=agreement]").click();
+        $("[type=button]").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
 }
 
