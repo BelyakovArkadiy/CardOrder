07c3360555dd36d3ae695a95d5dda1f63ef9e835
BelyakovArkadiy <belyakovarkadiy@gmail.com>
2021-03-08 16:36:04 +0600

    modified:   src/test/java/ru/netology/web/CardOrderTest.java

--- src/test/java/ru/netology/web/CardOrderTest.java
+++ src/test/java/ru/netology/web/CardOrderTest.java
@@ -1,24 +1,31 @@
 package ru.netology.web;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.support.Color;
+
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.*;
 
 class CardOrderTest {
-    @Test
-    void shouldTest1() {
+
+@BeforeEach
+public void setUp() {
         open("http://localhost:9999");
+        }
+
+
+    @Test
+    void shouldTestAllFields() {
         $("[data-test-id=name] input").setValue("Василий Алибабаевич");
         $("[data-test-id=phone] input").setValue("+79270000000");
         $("[data-test-id=agreement]").click();
         $("[type=button]").click();
-        $("[data-test-id=order-success]").shouldHave(exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
+
     @Test
-    void shouldTest2() {
-        open("http://localhost:9999");
+    void shouldTestNameInLatin() {
         $("[data-test-id=name] input").setValue("John Doe");
         $("[data-test-id=phone] input").setValue("+79270000000");
         $("[data-test-id=agreement]").click();
@@ -27,24 +34,39 @@ class CardOrderTest {
     }
 
     @Test
-    void shouldTest3() {
-        open("http://localhost:9999");
+    void shouldTestPhoneIsNotValid() {
         $("[data-test-id=name] input").setValue("Василий Алибабаевич");
         $("[data-test-id=phone] input").setValue("+792700000000");
         $("[data-test-id=agreement]").click();
         $("[type=button]").click();
         $("[data-test-id=phone] [class =input__sub]").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
+
     @Test
-    void shouldTest4() {
-        open("http://localhost:9999");
+    void shouldTestWithoutCheckbox() {
         $("[data-test-id=name] input").setValue("Василий Алибабаевич");
         $("[data-test-id=phone] input").setValue("+79270000000");
-       /* $("[data-test-id=agreement]").click();*/
         $("[type=button]").click();
-        String actual = $(".input_invalid").getCssValue("color");
-        String expected = "#ff5c5c";
-        org.junit.jupiter.api.Assertions.assertEquals(actual, Color.fromString(expected).asRgba());
+        $("[class=checkbox__text]").shouldHave(exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
+    }
+
+    @Test
+    void shouldTestWithoutName() {
+        $("[data-test-id=name] input").setValue("");
+        $("[data-test-id=phone] input").setValue("+79270000000");
+        $("[data-test-id=agreement]").click();
+        $("[type=button]").click();
+        $("[data-test-id=name] [class =input__sub]").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldTestWithoutPhone() {
+        $("[data-test-id=name] input").setValue("Василий Алибабаевич");
+        $("[data-test-id=phone] input").setValue("");
+        $("[data-test-id=agreement]").click();
+        $("[type=button]").click();
+        $("[data-test-id=phone] [class=input__sub]").shouldHave(exactText("Поле обязательно для заполнения"));
+
     }
 }
 
